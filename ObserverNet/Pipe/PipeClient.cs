b10d0ca4e7a1557e648dc28e09384d58fa88c394
jinyuttt <jinyu20072787@sina.com>
2019-12-02 23:37:18 +0800

    Signed-off-by: jinyuttt <jinyu20072787@sina.com>

---
+++ ObserverNet/Pipe/PipeClient.cs
@@ -0,0 +1,40 @@
+﻿#region << 版 本 注 释 >>
+/*----------------------------------------------------------------
+* 项目名称 ：ObserverDDS
+* 项目描述 ：
+* 类 名 称 ：PipeClient
+* 类 描 述 ：
+* 所在的域 ：DESKTOP-1IBOINI
+* 命名空间 ：ObserverDDS
+* 机器名称 ：DESKTOP-1IBOINI 
+* CLR 版本 ：4.0.30319.42000
+* 作    者 ：jinyu
+* 创建时间 ：2019
+* 更新时间 ：2019
+* 版 本 号 ：v1.0.0.0
+*******************************************************************
+* Copyright @ jinyu 2019. All rights reserved.
+*******************************************************************
+//----------------------------------------------------------------*/
+#endregion
+
+
+
+using System.IO;
+using System.IO.Pipes;
+using System.Security.Principal;
+
+namespace ObserverDDS
+{
+    public class PipeClient
+    {
+        public void Connect()
+        {
+            var pipeClient = new NamedPipeClientStream(".", "ObserverDDS", PipeDirection.InOut, PipeOptions.Asynchronous, TokenImpersonationLevel.None);
+            pipeClient.Connect(5000);
+            var swClient = new StreamWriter(pipeClient);
+            var srClient = new StreamReader(pipeClient);
+            swClient.AutoFlush = true;
+        }
+    }
+}
---
+++ ObserverNet/Pipe/PipeServer.cs
@@ -0,0 +1,60 @@
+﻿#region << 版 本 注 释 >>
+/*----------------------------------------------------------------
+* 项目名称 ：ObserverDDS
+* 项目描述 ：
+* 类 名 称 ：P
+* 类 描 述 ：
+* 所在的域 ：DESKTOP-1IBOINI
+* 命名空间 ：ObserverDDS
+* 机器名称 ：DESKTOP-1IBOINI 
+* CLR 版本 ：4.0.30319.42000
+* 作    者 ：jinyu
+* 创建时间 ：2019
+* 更新时间 ：2019
+* 版 本 号 ：v1.0.0.0
+*******************************************************************
+* Copyright @ jinyu 2019. All rights reserved.
+*******************************************************************
+//----------------------------------------------------------------*/
+#endregion
+
+
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Pipes;
+using System.Text;
+using System.Threading;
+
+namespace ObserverDDS
+{
+   public class PipeServer
+    {
+        public string RspMessage { get; set; }
+        private volatile bool isRun = true;
+        public void Start()
+        {
+            Thread srv = new Thread(new ParameterizedThreadStart((obj) =>
+              {
+                  using (NamedPipeServerStream pipeServer = new NamedPipeServerStream("ObserverDDS", PipeDirection.InOut))
+                  {
+                      while (isRun)
+                      {
+                          pipeServer.WaitForConnection();
+
+                        
+                          StreamReader sr = new StreamReader(pipeServer);
+                          sr.ReadToEnd();
+                          using (StreamWriter sw = new StreamWriter(pipeServer))
+                          {
+                              sw.AutoFlush = true;
+                              sw.WriteLine(RspMessage);
+                              sw.Flush();
+                          }
+                      }
+                  }
+              }));
+        }
+    }
+}
